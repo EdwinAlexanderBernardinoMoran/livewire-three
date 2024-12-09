807e765fa6daabec00cd7cb0302bc8d1d3b04dda
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2024-12-09 15:31:29 -0600

    Add Category and Post models with migrations, factories, and form component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             {{-- @livewire('create-post', ['title' => 'Create Post', 'user' => 1]) --}}
             {{-- @livewire('counter') --}}
-            @livewire('countries')
+            {{-- @livewire('countries') --}}
+            @livewire('form')
         </div>
     </div>
 </x-app-layout>
---
+++ resources/views/livewire/form.blade.php
@@ -0,0 +1,17 @@
+<div>
+    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <div class="bg-white shadow rounded-lg p-6">
+        <form>
+            <div class="mb-4">
+                <x-label>Name</x-label>
+                <x-input class="w-full" />
+            </div>
+
+            <div class="mb-4">
+                <x-label>Content</x-label>
+                <x-textarea class="w-full">
+                </x-textarea>
+            </div>
+        </form>
+    </div>
+</div>
